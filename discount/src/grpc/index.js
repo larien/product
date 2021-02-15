5bc8e8f54d28b9baad90f0c6eefaf0b7e99c129d
Lauren Ferreira <lauren.ferremch@gmail.com>
2021-02-15 16:54:01 -0300

    Implement grpc

---
+++ discount/src/grpc/index.js
@@ -0,0 +1,41 @@
+let grpc = require('grpc');
+let protoLoader = require('@grpc/proto-loader');
+let discount = require('../discount/discount');
+
+const protoPath = __dirname + '/protos/discount.proto';
+const host = process.env.GRPC_HOST || '0.0.0.0';
+const port = process.env.GRPC_PORT || ':50051';
+const address = host + port
+
+let packageDefinition = protoLoader.loadSync(
+    protoPath,
+    {keepCase: true,
+     longs: String,
+     enums: String,
+     defaults: true,
+     oneofs: true
+    });
+let discountProto = grpc.loadPackageDefinition(packageDefinition).protos;
+let server = new grpc.Server();
+
+server.addService(discountProto.DiscountService.service, {
+    discount: async (call, callback) => {
+        let productID = call.request.productID;
+        let userID = call.request.userID;
+        try {
+            let result = await discount.getPercentage(productID, userID);
+            callback(null, {
+                percentage: result,
+            });
+        } catch(err){
+            console.log("failed to get percentage" + err);
+        };
+    },
+});
+
+module.exports.start = function(){
+    server.bindAsync(address, grpc.ServerCredentials.createInsecure(), () => {
+        server.start();
+        console.log("Server running at " + port);
+    });
+};
