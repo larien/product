76417eb09932032b1994d932d699fbf9f6444596
Lauren Ferreira <lauren.ferremch@gmail.com>
2021-02-15 16:27:26 -0300

    Discount repository's Get method implementation with unit mocked tests

--- product/repository/discount/discount.go
+++ product/repository/discount/discount.go
@@ -3,13 +3,14 @@ package discount
 import (
 	"context"
 
-	protobuf "github.com/larien/product-service/protos"
+	protobuf "github.com/larien/product/protos"
 
 	grpc "google.golang.org/grpc"
 )
 
 // Discount represents the methods available in this repository layer
 type Discount interface {
+	Get(ctx context.Context, productID, userID string) (int64, error)
 }
 
 // Client represents the methods used by this repository layer
@@ -30,3 +31,19 @@ func New(client Client) Discount {
 	return &repository{client}
 }
 
+// Get obtains a product by its ID from the database
+func (r *repository) Get(ctx context.Context, productID, userID string) (int64, error) {
+	if userID == "" {
+		return 0, nil
+	}
+
+	req := &protobuf.DiscountRequest{
+		ProductID: productID,
+		UserID:    userID,
+	}
+	res, err := r.Client.Discount(ctx, req)
+	if err != nil {
+		return 0, err
+	}
+	return res.Percentage, nil
+}
---
+++ product/repository/discount/discount_test.go
@@ -0,0 +1,68 @@
+package discount_test
+
+import (
+	"errors"
+	"testing"
+
+	faker "github.com/bxcodec/faker/v3"
+	"github.com/larien/product/product/repository/discount"
+	serverMock "github.com/larien/product/product/repository/discount/mock"
+	protobuf "github.com/larien/product/protos"
+	"github.com/stretchr/testify/require"
+)
+
+func TestGet(t *testing.T) {
+	t.Parallel()
+	t.Run("user ID is empty", func(t *testing.T) {
+		t.Parallel()
+		is := require.New(t)
+
+		d := discount.New(nil)
+		percentage, err := d.Get(nil, "", "")
+
+		is.Equal(percentage, int64(0))
+		is.Nil(err)
+	})
+	t.Run("failed to obtain discount", func(t *testing.T) {
+		t.Parallel()
+		is := require.New(t)
+
+		s := serverMock.New()
+
+		req := &protobuf.DiscountRequest{
+			ProductID: faker.UUIDDigit(),
+			UserID:    faker.UUIDDigit(),
+		}
+		expectedError := errors.New(faker.Sentence())
+		s.On("Discount", nil, req).Return(nil, expectedError).Once()
+
+		d := discount.New(s)
+		percentage, err := d.Get(nil, req.ProductID, req.UserID)
+
+		s.AssertExpectations(t)
+		is.Equal(percentage, int64(0))
+		is.Contains(err.Error(), expectedError.Error())
+	})
+	t.Run("obtained discount with success", func(t *testing.T) {
+		t.Parallel()
+		is := require.New(t)
+
+		s := serverMock.New()
+
+		req := &protobuf.DiscountRequest{
+			ProductID: faker.UUIDDigit(),
+			UserID:    faker.UUIDDigit(),
+		}
+		res := &protobuf.DiscountResponse{
+			Percentage: 10,
+		}
+		s.On("Discount", nil, req).Return(res, nil).Once()
+
+		d := discount.New(s)
+		percentage, err := d.Get(nil, req.ProductID, req.UserID)
+
+		s.AssertExpectations(t)
+		is.Equal(percentage, res.Percentage)
+		is.Nil(err)
+	})
+}
