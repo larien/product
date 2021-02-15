9972c341d35a592320f62da864bdd274fb8bc2fe
Lauren Ferreira <lauren.ferremch@gmail.com>
2021-02-15 16:23:26 -0300

    Implement grpc driver

---
+++ product/drivers/grpc/grpc.go
@@ -0,0 +1,14 @@
+package grpc
+
+import (
+	"github.com/larien/product/product/drivers/config"
+	"google.golang.org/grpc"
+)
+
+// Connection is an alias for the gRPC connection that represents the connection from the gRPC client to the server
+type Connection = grpc.ClientConn
+
+// Dial is a wrapper for the Dial's gRPC function with a standard transport security option
+func Dial(config *config.GRPC) (*Connection, error) {
+	return grpc.Dial(config.Host+config.Port, grpc.WithInsecure()) // TODO - secure
+}
